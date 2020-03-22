789d86d74469c91d8891198f84541ed3b783b9ad
huimin.tan <tan.huimin26@gmail.com>
2020-03-23 01:03:50 +0800

    add in unit test for get matching line in searchStationService

--- MRT_UnitTest/Services/SearchStationServiceOthersTest.cs
+++ MRT_UnitTest/Services/SearchStationServiceOthersTest.cs
@@ -1,6 +1,7 @@
 ﻿using NUnit.Framework;
 using MRT.Services;
-using System;
+using System.Collections.Generic;
+using System.Linq;
 namespace MRT_UnitTest.Services
 {
     [TestFixture]
@@ -16,17 +17,29 @@ namespace MRT_UnitTest.Services
         [Test]
         public void TestGetMatchingLines_OneMatchingLine()
         {
-
+            List<string> prevStationCodes = new List<string> { "CC1", "TE2", "EW3"};
+            List<string> nextStationCodes = new List<string> { "NS1", "TE4"};
+            HashSet<string> matchingLines = searchStationService.GetMatchingLines(prevStationCodes, nextStationCodes);
+            Assert.AreEqual(1, matchingLines.Count);
+            Assert.AreEqual("TE", matchingLines.First());
         }
 
         [Test]
         public void TestGetMatchingLines_ManyMatchingLine()
         {
+            List<string> prevStationCodes = new List<string> { "CC1", "TE2", "EW3" };
+            List<string> nextStationCodes = new List<string> { "EW1", "TE4" };
+            HashSet<string> matchingLines = searchStationService.GetMatchingLines(prevStationCodes, nextStationCodes);
+            Assert.AreEqual(2, matchingLines.Count);
         }
 
         [Test]
         public void TestGetMatchingLines_NoMatchingLine()
         {
+            List<string> prevStationCodes = new List<string> { "CC1", "TE2", "EW3" };
+            List<string> nextStationCodes = new List<string> { "PP1", "AA4" };
+            HashSet<string> matchingLines = searchStationService.GetMatchingLines(prevStationCodes, nextStationCodes);
+            Assert.AreEqual(0, matchingLines.Count);
         }
     }
 }

f3ec4310e2049fb2304ca11d84db99cd25d79575
huimin.tan <tan.huimin26@gmail.com>
2020-03-22 23:52:02 +0800

    refactor routestation

---
+++ MRT/Models/RouteStation.cs
@@ -0,0 +1,16 @@
+﻿namespace MRT.Models
+{
+    public class RouteStation
+    {
+        public RouteStation() { }
+        public RouteStation(Station station)
+        {
+            Station = station;
+            NextStation = null;
+        }
+
+        public Station Station { get; set; }
+        public RouteStation NextStation { get; set; }
+        public RouteStation PrevStation { get; set; }
+    }
+}
